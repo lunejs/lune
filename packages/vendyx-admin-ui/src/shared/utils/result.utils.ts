e2c387b605deb87f83d31763af4c35f4718099ac
Samuel <samuel.corrales621@gmail.com>
2025-10-27 22:43:43 -0700

    feat(admin-ui): ActionResult supports data

--- packages/vendyx-admin-ui/src/shared/utils/result.utils.ts
+++ packages/vendyx-admin-ui/src/shared/utils/result.utils.ts
@@ -1,8 +1,6 @@
-export type ActionResult<TErrorCode = string, TData = unknown> =
-  | {
-      isSuccess: true;
-      data: TData;
-    }
+export type ActionResult<TErrorCode = string, TData = void> =
+  // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
+  | (TData extends void ? { isSuccess: true } : { isSuccess: true; data: TData })
   | {
       isSuccess: false;
       error: string;
