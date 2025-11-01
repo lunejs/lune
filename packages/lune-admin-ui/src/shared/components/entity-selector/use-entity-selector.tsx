f5f44c9ce0f9b9eb3fa38753ed1e720044807581
Samuel <samuel.corrales621@gmail.com>
2025-11-01 13:57:02 -0700

    fix(admin-ui): remove reset selection when default selected changes in entity selector

--- packages/lune-admin-ui/src/shared/components/entity-selector/use-entity-selector.tsx
+++ packages/lune-admin-ui/src/shared/components/entity-selector/use-entity-selector.tsx
@@ -1,13 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import type { EntitySelectorProps } from './entity-selector';
 
 export const useEntitySelector = <T,>(input: Input<T>) => {
   const [selected, setSelected] = useState<T[]>(input.defaultSelected);
-
-  useEffect(() => {
-    setSelected([...input.defaultSelected, ...selected]);
-  }, [input.defaultSelected]);
 
   const isSelected = (item: T) => {
     return selected.some(s => input.getRowId(s) === input.getRowId(item));
