1002a1eb369ea46aae2333a045812f3afab4c061
Samuel <samuel.corrales621@gmail.com>
2025-11-01 21:14:40 -0700

    feat(core): add state table

---
+++ packages/lune-core/database/migrations/20251102040912_create_state_table.ts
@@ -0,0 +1,22 @@
+import type { Knex } from 'knex';
+
+const TABLE_NAME = 'state';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable(TABLE_NAME, table => {
+    table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
+    table.timestamp('created_at', { useTz: true }).notNullable().defaultTo(knex.fn.now());
+    table.timestamp('updated_at', { useTz: true }).notNullable().defaultTo(knex.fn.now());
+
+    table.string('name', 255).notNullable();
+    table.specificType('code', 'char(5)').notNullable().unique();
+
+    table.uuid('country_id').notNullable().references('id').inTable('country');
+
+    table.unique(['country_id', 'code']);
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTableIfExists(TABLE_NAME);
+}
