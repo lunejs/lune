d09b1a789b6fdb7105e356568f7d58cfdeba354c
Samuel <samuel.corrales621@gmail.com>
2025-11-01 20:03:46 -0700

    feat(core): add customer serializer

---
+++ packages/lune-core/src/persistence/serializers/customer.serializer.ts
@@ -0,0 +1,19 @@
+import type { Customer, CustomerTable } from '../entities/customer';
+
+import { Serializer } from './serializer';
+
+export class CustomerSerializer extends Serializer<Customer, CustomerTable> {
+  constructor() {
+    super([
+      ['id', 'id'],
+      ['created_at', 'createdAt'],
+      ['updated_at', 'updatedAt'],
+      ['first_name', 'firstName'],
+      ['last_name', 'lastName'],
+      ['email', 'email'],
+      ['password', 'password'],
+      ['phone_number', 'phoneNumber'],
+      ['enabled', 'enabled']
+    ]);
+  }
+}
