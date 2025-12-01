792ac1e1ded3cb7cd46de02617b5c6337a35671c
Samuel <samuel.corrales621@gmail.com>
2025-12-01 13:35:41 -0700

    feat(admin-ui): add discount operations

---
+++ packages/lune-admin-ui/src/lib/api/operations/discount.operations.ts
@@ -0,0 +1,44 @@
+import { graphql } from '../codegen';
+
+export const COMMON_DISCOUNT_HANDLER_FRAGMENT = graphql(`
+  fragment CommonDiscountHandler on DiscountHandler {
+    code
+    name
+    description
+    args
+  }
+`);
+
+export const COMMON_LIST_DISCOUNT_FRAGMENT = graphql(`
+  fragment CommonListDiscount on Discount {
+    id
+    code
+    applicationMode
+    applicationLevel
+    startsAt
+    endsAt
+    enabled
+  }
+`);
+
+export const GET_ALL_DISCOUNTS_QUERY = graphql(`
+  query GetAllDiscounts($input: DiscountListInput!) {
+    discounts(input: $input) {
+      count
+      pageInfo {
+        total
+      }
+      items {
+        ...CommonListDiscount
+      }
+    }
+  }
+`);
+
+export const GET_ALL_DISCOUNT_HANDLERS_QUERY = graphql(`
+  query GetAllDiscountHandlers {
+    discountHandlers {
+      ...CommonDiscountHandler
+    }
+  }
+`);
