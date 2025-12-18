0da3fd2edfd4b7deed121a48c66840c088156218
Samuel <samuel.corrales621@gmail.com>
2025-12-18 13:17:48 -0700

    feat(email-plugin): add NodemailerSender

---
+++ packages/lune-email-plugin/src/senders/nodemailer/nodemailer.sender.ts
@@ -0,0 +1,35 @@
+import * as nodemailer from 'nodemailer';
+import { EmailData, EmailSender } from '../sender';
+
+/**
+ * Nodemailer sender interface.
+ *
+ * @description
+ * Send emails with nodemailer
+ */
+export class NodemailerSender implements EmailSender {
+  private readonly transporter: nodemailer.Transporter;
+
+  constructor(config: NodemailerSenderConfig) {
+    this.transporter = nodemailer.createTransport(config);
+  }
+
+  async send(email: EmailData): Promise<void> {
+    await this.transporter.sendMail({
+      from: `${email.from.name} <${email.from.email}>`,
+      to: email.to,
+      subject: email.subject,
+      html: email.html,
+    });
+  }
+}
+
+export type NodemailerSenderConfig = {
+  host: string;
+  port: number;
+  secure: boolean;
+  auth: {
+    user: string;
+    pass: string;
+  };
+};
