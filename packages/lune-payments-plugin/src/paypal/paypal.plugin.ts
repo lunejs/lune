aa4168160b9a086f7b4c3bc4a81aa88e79527101
Samuel <samuel.corrales621@gmail.com>
2025-12-20 01:40:30 -0700

    feat(payments-plugin): add jsdoc for PaypalPlugin

--- packages/lune-payments-plugin/src/paypal/paypal.plugin.ts
+++ packages/lune-payments-plugin/src/paypal/paypal.plugin.ts
@@ -6,6 +6,99 @@ import { PayPal } from './adapters/paypal';
 import { PayPalResolver } from './api/paypal.resolver';
 import { paypalHandler } from './paypal.handler';
 
+/**
+ * Paypal plugin
+ *
+ * @description
+ * Plugin to integrate [Paypal](https://paypal.com) into lune
+ *
+ * ### Requirements
+ * 1. Set Up a PayPal Business Account
+ * 2. Create a PayPal App in the [PayPal Developer Dashboard](https://developer.paypal.com)
+ * 3. Get your PayPal Client ID and Secret
+ *
+ * ### Admin UI usage
+ * 1. Add plugin to your lune config `plugins` array
+ * ```ts
+ * // ...
+ * plugins: [
+ *   new PaypalPlugin({
+ *     clientId: 'YOUR_PAYPAL_CLIENT_ID',
+ *     secret: 'YOUR_PAYPAL_SECRET',
+ *     devMode: false // Set to true if you want to use paypal in sandbox mode
+ *   })
+ * ]
+ * ```
+ *
+ * 2. Create a new Payment method in the admin ui and select PayPal as the handler.
+ *
+ * ### Storefront usage
+ *
+ * This plugin is designed to work with the [Standard Checkout](https://developer.paypal.com/studio/checkout/standard) flow.
+ *
+ * 1. Add `createPaypalOrder` mutation to your storefront schema. This mutation will create a new paypal order.
+ * ```graphql
+ * mutation CreatePaypalOrder($cartId: ID!) {
+ *   createPaypalOrder(orderId: $cartId) {
+ *     apiErrors {
+ *       code
+ *       message
+ *     }
+ *     orderId
+ *   }
+ * }
+ * ```
+ *
+ * 2. Install `@paypal/react-paypal-js` package
+ * ```bash
+ * yarn add \@paypal/react-paypal-js
+ * ```
+ *
+ * 3. Create a PaypalButton component
+ * ```tsx
+ * // paypal-button.tsx
+ * import { PayPalButtons, PayPalScriptProvider } from '@paypal/react-paypal-js';
+ *
+ * export const PaypalButton = ({orderId}: {orderId: string}) => {
+ *   return (
+ *     <PayPalScriptProvider
+ *       options={{
+ *         clientId: 'YOUR_PAYPAL_CLIENT_ID',
+ *         components: 'buttons',
+ *         currency: 'YOUR_STORE_CURRENCY' // Example: 'USD' | 'MXN'
+ *       }}
+ *     >
+ *       <PayPalButtons
+ *         style={{
+ *           color: 'blue',
+ *           layout: 'horizontal',
+ *           tagline: false,
+ *         }}
+ *         createOrder={async () => {
+ *           // replace with your `createPaypalOrder` graphql mutation
+ *           const result = await createPaypalOrder(orderId)
+ *
+ *           return result.orderId
+ *         }}
+ *         onApprove={async (data) => {
+ *           // replace with your `addPaymentToOrder` graphql mutation
+ *           const result = await addPaymentToOrder(
+ *            orderId,
+ *            {
+ *              method: methodId,
+ *              metadata: { paypalOrderId: data.orderID }
+ *            })
+ *
+ *           // Redirect to the order confirmation page or show a success message
+ *         }}
+ *       >
+ *
+ *       </PayPalButtons>
+ *     </PayPalScriptProvider>
+ *   )
+ * }
+ * ```
+ */
 export class PaypalPlugin extends LunePlugin {
   constructor(config: PaypalPluginConfig) {
     PayPal.init(config);
