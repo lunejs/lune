d0588e45632cc31918975ccd7c0a0d42e1f15920
Samuel <samuel.corrales621@gmail.com>
2025-12-20 18:15:32 -0700

    tests(core): add tests for customFieldDefinitions

---
+++ packages/lune-core/tests/admin/resolvers/custom-field-definition/custom-field-definitions/custom-field-definitions.test.ts
@@ -0,0 +1,145 @@
+import request from 'supertest';
+
+import { LuneServer } from '@/server';
+import { TEST_LUNE_CONFIG } from '@/tests/utils/test-config';
+import { TestUtils } from '@/tests/utils/test-utils';
+
+import { CustomFieldDefinitionFixtures } from './fixtures/custom-field-definition.fixtures';
+import { ShopConstants, ShopFixtures } from './fixtures/shop.fixtures';
+import { UserConstants, UserFixtures } from './fixtures/user.fixtures';
+
+describe('customFieldDefinitions - Query', () => {
+  const testHelper = new TestUtils();
+
+  const luneServer = new LuneServer(TEST_LUNE_CONFIG);
+  const app = luneServer.getApp();
+
+  beforeEach(async () => {
+    await testHelper.loadFixtures([
+      new UserFixtures(),
+      new ShopFixtures(),
+      new CustomFieldDefinitionFixtures()
+    ]);
+  });
+
+  afterEach(async () => {
+    await testHelper.resetDatabase();
+  });
+
+  afterAll(async () => {
+    await testHelper.destroyDatabase();
+    await luneServer.teardown();
+  });
+
+  test('returns all custom field definitions', async () => {
+    const res = await request(app)
+      .post('/admin-api')
+      .set('Authorization', `Bearer ${UserConstants.AccessToken}`)
+      .set('x_lune_shop_id', ShopConstants.ID)
+      .send({
+        query: GET_CUSTOM_FIELD_DEFINITIONS_QUERY,
+        variables: {}
+      });
+
+    const { customFieldDefinitions } = res.body.data;
+
+    expect(customFieldDefinitions.items).toHaveLength(4);
+    expect(customFieldDefinitions.count).toBe(4);
+    expect(customFieldDefinitions.pageInfo.total).toBe(4);
+  });
+
+  test('returns limited number of custom field definitions', async () => {
+    const res = await request(app)
+      .post('/admin-api')
+      .set('Authorization', `Bearer ${UserConstants.AccessToken}`)
+      .set('x_lune_shop_id', ShopConstants.ID)
+      .send({
+        query: GET_CUSTOM_FIELD_DEFINITIONS_QUERY,
+        variables: {
+          input: {
+            take: 2
+          }
+        }
+      });
+
+    const { customFieldDefinitions } = res.body.data;
+
+    expect(customFieldDefinitions.items).toHaveLength(2);
+    expect(customFieldDefinitions.count).toBe(2);
+    expect(customFieldDefinitions.pageInfo.total).toBe(4);
+  });
+
+  test('returns custom field definitions with offset', async () => {
+    const res = await request(app)
+      .post('/admin-api')
+      .set('Authorization', `Bearer ${UserConstants.AccessToken}`)
+      .set('x_lune_shop_id', ShopConstants.ID)
+      .send({
+        query: GET_CUSTOM_FIELD_DEFINITIONS_QUERY,
+        variables: {
+          input: {
+            skip: 2
+          }
+        }
+      });
+
+    const { customFieldDefinitions } = res.body.data;
+
+    expect(customFieldDefinitions.items).toHaveLength(2);
+    expect(customFieldDefinitions.count).toBe(2);
+    expect(customFieldDefinitions.pageInfo.total).toBe(4);
+  });
+
+  test('returns custom field definitions with pagination applied', async () => {
+    const res = await request(app)
+      .post('/admin-api')
+      .set('Authorization', `Bearer ${UserConstants.AccessToken}`)
+      .set('x_lune_shop_id', ShopConstants.ID)
+      .send({
+        query: GET_CUSTOM_FIELD_DEFINITIONS_QUERY,
+        variables: {
+          input: {
+            take: 2,
+            skip: 1
+          }
+        }
+      });
+
+    const { customFieldDefinitions } = res.body.data;
+
+    expect(customFieldDefinitions.items).toHaveLength(2);
+    expect(customFieldDefinitions.count).toBe(2);
+    expect(customFieldDefinitions.pageInfo.total).toBe(4);
+  });
+
+  test('returns Authorization error when no token is provided', async () => {
+    const response = await request(app).post('/admin-api').send({
+      query: GET_CUSTOM_FIELD_DEFINITIONS_QUERY,
+      variables: {}
+    });
+
+    expect(response.body.errors[0].extensions.code).toBe('UNAUTHORIZED');
+  });
+});
+
+const GET_CUSTOM_FIELD_DEFINITIONS_QUERY = /* GraphQL */ `
+  query CustomFieldDefinitions($input: ListInput) {
+    customFieldDefinitions(input: $input) {
+      items {
+        id
+        createdAt
+        updatedAt
+        name
+        key
+        isList
+        appliesToEntity
+        type
+        metadata
+      }
+      count
+      pageInfo {
+        total
+      }
+    }
+  }
+`;
---
+++ packages/lune-core/tests/admin/resolvers/custom-field-definition/custom-field-definitions/fixtures/custom-field-definition.fixtures.ts
@@ -0,0 +1,65 @@
+import {
+  type CustomFieldDefinitionTable,
+  CustomFieldType
+} from '@/persistence/entities/custom-field-definition';
+import { Tables } from '@/persistence/tables';
+import type { Fixture } from '@/tests/utils/fixtures';
+import { TestUtils } from '@/tests/utils/test-utils';
+
+import { ShopConstants } from './shop.fixtures';
+
+export const CustomFieldDefinitionConstants = {
+  TextFieldID: TestUtils.generateUUID(),
+  MultiLineTextFieldID: TestUtils.generateUUID(),
+  IntegerFieldID: TestUtils.generateUUID(),
+  ReferenceFieldID: TestUtils.generateUUID()
+};
+
+export class CustomFieldDefinitionFixtures implements Fixture<CustomFieldDefinitionTable> {
+  table = Tables.CustomFieldDefinition;
+
+  async build(): Promise<Partial<CustomFieldDefinitionTable>[]> {
+    return [
+      {
+        id: CustomFieldDefinitionConstants.TextFieldID,
+        name: 'Short Description',
+        key: 'short_description',
+        is_list: false,
+        applies_to_entity: 'product',
+        type: CustomFieldType.SingleLineText,
+        metadata: null,
+        shop_id: ShopConstants.ID
+      },
+      {
+        id: CustomFieldDefinitionConstants.MultiLineTextFieldID,
+        name: 'Long Description',
+        key: 'long_description',
+        is_list: false,
+        applies_to_entity: 'product',
+        type: CustomFieldType.MultiLineText,
+        metadata: null,
+        shop_id: ShopConstants.ID
+      },
+      {
+        id: CustomFieldDefinitionConstants.IntegerFieldID,
+        name: 'Stock Count',
+        key: 'stock_count',
+        is_list: false,
+        applies_to_entity: 'product',
+        type: CustomFieldType.Integer,
+        metadata: null,
+        shop_id: ShopConstants.ID
+      },
+      {
+        id: CustomFieldDefinitionConstants.ReferenceFieldID,
+        name: 'Related Products',
+        key: 'related_products',
+        is_list: true,
+        applies_to_entity: 'product',
+        type: CustomFieldType.Reference,
+        metadata: { targetEntity: 'product' },
+        shop_id: ShopConstants.ID
+      }
+    ];
+  }
+}
