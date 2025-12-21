6f330b1f589eb52ab5a220e2ae033017200a91d3
Samuel <samuel.corrales621@gmail.com>
2025-12-21 12:28:25 -0700

    feat(admin-ui): image field supports list

--- packages/lune-admin-ui/src/lib/custom-fields/components/fields/image.tsx
+++ packages/lune-admin-ui/src/lib/custom-fields/components/fields/image.tsx
@@ -11,8 +11,10 @@ export const ImageCustomField = ({ definition, onChange }: Props) => {
   return (
     <AssetSelector
       onDone={assets => {
-        onChange(assets[0].id);
-        setAssets([assets[0]]);
+        const newAssets = definition.isList ? assets : [assets[0]];
+
+        onChange(newAssets.map(asset => asset.id));
+        setAssets(newAssets);
       }}
     >
       <div className="flex items-center gap-4">
@@ -22,13 +24,13 @@ export const ImageCustomField = ({ definition, onChange }: Props) => {
             'file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
             'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]',
             'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
-            'w-3/4 shrink-0 px-1'
+            'w-3/4 shrink-0 px-1 flex items-center gap-1 overflow-x-hidden'
           )}
         >
           {assets.map(asset => (
-            <Badge variant={'secondary'} className="bg-background">
+            <Badge key={asset.id} variant={'secondary'} className="bg-background">
               <img src={asset.source} alt={asset.filename} className="w-5 h-5 rounded" />
-              {asset.filename.length > 15 ? `${asset.filename.slice(0, 15)}...` : asset.filename}
+              {asset.filename.length > 10 ? `${asset.filename.slice(0, 10)}...` : asset.filename}
             </Badge>
           ))}
         </div>
@@ -38,6 +40,6 @@ export const ImageCustomField = ({ definition, onChange }: Props) => {
 };
 
 type Props = {
-  onChange: (assetId: string) => void;
+  onChange: (assetIds: string[]) => void;
   definition: CommonCustomFieldDefinitionFragment;
 };
