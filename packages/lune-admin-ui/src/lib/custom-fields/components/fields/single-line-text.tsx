992d5dbeb2755d44cc16275f422fb3c9348f364c
Samuel <samuel.corrales621@gmail.com>
2025-12-21 03:42:04 -0700

    feat(admin-ui): SingleLineTextCustomField supports list

--- packages/lune-admin-ui/src/lib/custom-fields/components/fields/single-line-text.tsx
+++ packages/lune-admin-ui/src/lib/custom-fields/components/fields/single-line-text.tsx
@@ -1,21 +1,23 @@
 import { useId, useState } from 'react';
+import { XIcon } from 'lucide-react';
 
-import { Input, Label, Popover, PopoverContent, PopoverTrigger } from '@lune/ui';
+import { Button, Input, Label, Popover, PopoverContent, PopoverTrigger } from '@lune/ui';
 
 import type { CommonCustomFieldDefinitionFragment } from '@/lib/api/types';
+import { genUUID } from '@/shared/utils/id.utils';
 
 export const SingleLineTextCustomField = ({ definition, onChange }: Props) => {
   const id = useId();
 
-  const [value, setValue] = useState('');
-  const [persistedValue, setPersistedValue] = useState('');
+  const [values, setValues] = useState<Value[]>([new Value('')]);
+  const [persistedValue, setPersistedValue] = useState<Value[]>([new Value('')]);
 
   return (
     <Popover
       onOpenChange={isOpen => {
         if (!isOpen) {
-          setPersistedValue(value);
-          onChange(value);
+          setPersistedValue(values);
+          onChange(values.map(v => v.value));
         }
       }}
     >
@@ -24,13 +26,52 @@ export const SingleLineTextCustomField = ({ definition, onChange }: Props) => {
           {definition.name}
         </Label>
         <PopoverTrigger asChild>
-          <Input id={id} value={persistedValue} readOnly className="w-3/4 shrink-0 text-start" />
+          <Input
+            id={id}
+            value={persistedValue.map(v => v.value).join(', ')}
+            readOnly
+            className="w-3/4 shrink-0 text-start"
+          />
         </PopoverTrigger>
       </div>
       <PopoverContent className="w-72" align="start">
         <div className="flex flex-col gap-2">
           <Label>{definition.name}</Label>
-          <Input defaultValue={persistedValue} onChange={e => setValue(e.target.value)} />
+          {values.map(value => {
+            const defaultValue = persistedValue.find(v => v.id === value.id);
+
+            return (
+              <div key={value.id} className="flex items-center gap-2">
+                <Input
+                  defaultValue={defaultValue?.value}
+                  onChange={e =>
+                    setValues(
+                      values.map(v => (v.id === value.id ? { ...v, value: e.target.value } : v))
+                    )
+                  }
+                />
+                {values.length > 1 && (
+                  <Button
+                    variant={'ghost'}
+                    size={'icon'}
+                    onClick={() => setValues(prev => prev.filter(v => v.id !== value.id))}
+                  >
+                    <XIcon />
+                  </Button>
+                )}
+              </div>
+            );
+          })}
+          {definition.isList && (
+            <Button
+              variant={'outline'}
+              size={'sm'}
+              onClick={() => setValues(prev => [...prev, new Value('')])}
+              className="w-fit"
+            >
+              Add item
+            </Button>
+          )}
         </div>
       </PopoverContent>
     </Popover>
@@ -38,6 +79,14 @@ export const SingleLineTextCustomField = ({ definition, onChange }: Props) => {
 };
 
 type Props = {
-  onChange: (value: string) => void;
+  onChange: (value: string[]) => void;
   definition: CommonCustomFieldDefinitionFragment;
 };
+
+class Value {
+  public id: string;
+
+  constructor(public value: string) {
+    this.id = genUUID();
+  }
+}
---
+++ packages/lune-admin-ui/src/shared/utils/id.utils.ts
@@ -0,0 +1,1 @@
+export const genUUID = () => crypto.randomUUID();
