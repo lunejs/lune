d1913f89821e0d6e1733d12f1c4f76a78f1b43f5
Samuel <samuel.corrales621@gmail.com>
2025-12-22 20:19:39 -0700

    feat(core): add collection_custom_field table

---
+++ packages/lune-core/database/migrations/20251223031750_create_collection_custom_field_table.ts
@@ -0,0 +1,29 @@
+import type { Knex } from 'knex';
+
+const TABLE_NAME = 'collection_custom_field';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable(TABLE_NAME, table => {
+    table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
+    table.timestamp('created_at', { useTz: true }).notNullable().defaultTo(knex.fn.now());
+    table.timestamp('updated_at', { useTz: true }).notNullable().defaultTo(knex.fn.now());
+
+    table.jsonb('value').notNullable();
+
+    table.uuid('collection_id').notNullable().references('id').inTable('collection');
+    table.uuid('definition_id').notNullable().references('id').inTable('custom_field_definition');
+
+    table
+      .uuid('shop_id')
+      .notNullable()
+      .defaultTo(knex.raw(`(current_setting('app.current_shop_id'::text))::uuid`))
+      .references('id')
+      .inTable('shop');
+
+    table.unique(['shop_id', 'collection_id', 'definition_id']);
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTableIfExists(TABLE_NAME);
+}
